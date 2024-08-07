f46155960ef965cbacd557a98ff92730b0331e6b
Abdellah Kaissari <abdellah.kaissari@kyriba.com>
2024-08-07 23:01:46 +0200

    typo

--- simple_rnn_imdb/main.py
+++ simple_rnn_imdb/main.py
@@ -5,6 +5,8 @@ from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
 import os
+import streamlit as st
+
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
@@ -26,7 +28,6 @@ def preprocess_text(text):
     return padded_review
 
 
-import streamlit as st
 ## streamlit app
 # Streamlit app
 st.title('IMDB Movie Review Sentiment Analysis')
